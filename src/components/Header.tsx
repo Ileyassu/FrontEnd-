650ecd727c00b3f64eb72d5e35a1a30868f40223
Ileyassu <ilyas.benaissa@um5r.ac.ma>
2025-11-22 17:59:07 +0100

    adjust header styles for improved layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,9 +11,9 @@ export function Header (props : HeaderProps = {}) : HTMLElement {
 
     return (
         <header class="absolute top-0 left-0 right-0 z-50 bg-transparent">
-            <nav class="px-35 py-15 mx-auto flex items-center justify-between">
+            <nav class="px-35 py-10 mx-auto flex items-center justify-between">
                 <a href="/">
-                    <img class=" w-55" src= "/media/logo/logo.png" alt="logo"/>
+                    <img class=" w-40" src= "/media/logo/logo.png" alt="logo"/>
                 </a> 
                 <div class="flex gap-6">
                     {links.map(link => (
